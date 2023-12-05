559a4bb9dffe4b18a111a667cdb019d46bcc76c7
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2023-12-05 15:50:55 +0100

    Add Logger

---
+++ Beacon/logger.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifdef _DEBUG
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define LOG(color, label, format, ...) \
+    do { \
+        printf("%s%s " ANSI_COLOR_RESET, color, label); \
+        printf(format, ##__VA_ARGS__); \
+        printf("\n"); \
+    } while (0)
+
+
+#define LINFO(...) LOG(ANSI_COLOR_BLUE, "[INFO]", __VA_ARGS__)
+#define LWARNING(...) LOG(ANSI_COLOR_YELLOW, "[WARNING]", __VA_ARGS__)
+#define LERROR(...) LOG(ANSI_COLOR_RED, "[ERROR]", __VA_ARGS__)
+#define LOK(...) LOG(ANSI_COLOR_GREEN, "[OK]", __VA_ARGS__)
+#define LLOG(...) LOG("\n", "", __VA_ARGS__)
+#define LNEWLINE() LOG("\n")
+
+#else
+
+#define LINFO(...)
+#define LWARNING(...)
+#define LERROR(...)
+#define LOK(...)
+#define LLOG(...)
+#define LNEWLINE()
+
+#endif
