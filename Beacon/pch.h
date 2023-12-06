c13a611b17fc2badf7347bbba57767ae1d5a1b0b
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2023-12-06 05:10:31 +0100

    Update pch.h
    Update pch.h

--- Beacon/pch.h
+++ Beacon/pch.h
@@ -1,3 +1,18 @@
 #pragma once
 
+/*
+ * Include convention for this project:
+ * 1. Precompiled header
+ * 2. The ".h" file for the current source file
+ * 3. C standard library headers
+ * 4. Third-party library headers
+ * 5. Windows headers
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <time.h>
+
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
 #include <windows.h>
