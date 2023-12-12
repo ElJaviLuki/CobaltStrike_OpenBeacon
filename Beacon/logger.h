018a3cfe9df7dd3049578c4b5682eb620593069a
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2023-12-12 18:57:38 +0100

    Add LTODO to logger.h

--- Beacon/logger.h
+++ Beacon/logger.h
@@ -6,6 +6,7 @@
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
 #define ANSI_COLOR_BLUE    "\x1b[36m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
 #define LOG(color, label, format, ...) \
@@ -20,6 +21,7 @@
 #define LWARNING(...) LOG(ANSI_COLOR_YELLOW, "[WARNING]", __VA_ARGS__)
 #define LERROR(...) LOG(ANSI_COLOR_RED, "[ERROR]", __VA_ARGS__)
 #define LOK(...) LOG(ANSI_COLOR_GREEN, "[OK]", __VA_ARGS__)
+#define LTODO(...) LOG(ANSI_COLOR_MAGENTA, "[TODO]", __VA_ARGS__)
 #define LLOG(...) LOG("\n", "", __VA_ARGS__)
 #define LNEWLINE() LOG("\n")
 
@@ -29,6 +31,7 @@
 #define LWARNING(...)
 #define LERROR(...)
 #define LOK(...)
+#define LTODO(...)
 #define LLOG(...)
 #define LNEWLINE()
 
