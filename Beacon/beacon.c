1d32a34d2d59d94bdc9c1c1d7e84c5dfa8fe6319
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2023-12-27 22:31:20 +0100

    Implement BeaconFormat... formatp functions

--- Beacon/beacon.c
+++ Beacon/beacon.c
@@ -89,3 +89,71 @@ char* BeaconDataExtract(datap* parser, int* size)
 
 	return sb.buffer;
 }
+
+void BeaconFormatAlloc(formatp* format, int maxsz)
+{
+	char* buffer = (char*)malloc(maxsz);
+	*format = (formatp){ buffer, buffer, 0, maxsz };
+}
+
+void BeaconFormatReset(formatp* format)
+{
+	*format = (formatp){ format->original, format->original, 0, format->size };
+}
+
+void BeaconFormatAppend(formatp* format, char* text, int len)
+{
+	if(format->size - format->length >= len)
+		return;
+
+	if (len == 0)
+		return;
+
+	memcpy(format->buffer, text, len);
+	format->buffer += len;
+	format->length += len;
+}
+
+void BeaconFormatPrintf(formatp* format, char* fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+
+	int len = vsnprintf(format->buffer, format->size - format->length, fmt, args);
+	format->buffer += len;
+	format->length += len;
+
+	va_end(args);
+}
+
+char* BeaconFormatToString(formatp* format, int* size)
+{
+	if(!size)
+		return NULL;
+
+	*size = BeaconDataLength(format);
+	return BeaconDataOriginal(format);
+}
+
+void BeaconFormatFree(formatp* format)
+{
+	/* note: we don't force memzero the buffer explicitly, as free is already overwritten to do that */
+	free(format->original);
+}
+
+void BeaconFormatInt(formatp* format, int value)
+{
+	value = htonl(value);
+	BeaconFormatAppend(format, (char*)&value, sizeof(int));
+}
+
+void BeaconFormatShort(formatp* format, short value)
+{
+	value = htons(value);
+	BeaconFormatAppend(format, (char*)&value, sizeof(short));
+}
+
+void BeaconFormatChar(formatp* format, char value)
+{
+	BeaconFormatAppend(format, (char*)&value, sizeof(char));
+}
