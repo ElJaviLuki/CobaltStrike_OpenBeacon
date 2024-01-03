9f43546a98fa8bd9f185abdd5cc38c3f049561b5
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2024-01-03 20:50:45 +0100

    Implement COMMAND_SLEEP task

--- Beacon/self.c
+++ Beacon/self.c
@@ -5,9 +5,23 @@
 #include "beacon.h"
 
 int gSleepTime;
+int gJitter;
 
 void Die(void)
 {
 	gSleepTime = 0;
 	BeaconOutput(CALLBACK_DEAD, NULL, 0);
+}
+
+void SleepSet(char* buffer, int length)
+{
+	datap parser;
+	BeaconDataParse(&parser, buffer, length);
+
+	gSleepTime = BeaconDataInt(&parser);
+	int jitter = BeaconDataInt(&parser);
+	if (jitter >= 100)
+		jitter = 0;
+	gJitter = jitter;
+	BeaconDataZero(&parser);
 }
--- Beacon/self.h
+++ Beacon/self.h
@@ -1,3 +1,4 @@
 #pragma once
 
 void Die(void);
+void SleepSet(char* buffer, int length);
