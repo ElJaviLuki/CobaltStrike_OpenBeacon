2cfae40436c1d870944faf93be595d78b4956188
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2024-01-04 17:08:13 +0100

    Identity - Include gIdentityIsLoggedIn attribute

--- Beacon/identity.h
+++ Beacon/identity.h
@@ -1,5 +1,7 @@
 #pragma once
 
 extern HANDLE gIdentityToken;
+extern BOOL gIdentityIsLoggedIn;
+
 void IdentityConditionalRevert(BOOL ignoreToken);
 void IdentityConditionalImpersonate(BOOL ignoreToken);
