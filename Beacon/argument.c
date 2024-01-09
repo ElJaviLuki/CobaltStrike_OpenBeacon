cd5871386bae27683be8ed654ae61b057e024ae6
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2024-01-09 22:32:39 +0100

    Implement ArgumentFindOrCreate

--- Beacon/argument.c
+++ Beacon/argument.c
@@ -2,6 +2,8 @@
 
 #include "argument.h"
 
+#include "beacon.h"
+
 typedef struct _ARGUMENT_ENTRY
 {
 	BOOL isActive;
@@ -24,4 +26,33 @@ BOOL ArgumentFindMatch(EXPANDED_CMD* extendedCmd, const char* cmd)
 	}
 
 	return FALSE;
+}
+
+ARGUMENT_ENTRY* ArgumentFindOrCreate(char* expanded)
+{
+	for (ARGUMENT_ENTRY* current = gArguments; current != NULL; current = current->next)
+	{
+		if (!current->isActive && strcmp(expanded, current->expandedCmd) == 0)
+			return current;
+	}
+
+	ARGUMENT_ENTRY* current = gArguments;
+
+	while(current && current->isActive)
+		current = current->next;
+
+	ARGUMENT_ENTRY* argument;
+	if (!current)
+	{
+		// Create a new entry for the new argument
+		argument = (ARGUMENT_ENTRY*)malloc(sizeof(ARGUMENT_ENTRY));
+		*argument = (ARGUMENT_ENTRY){ .isActive = FALSE, .expandedCmd = NULL, .expandedFullCmd = NULL, .next = current };
+		gArguments = argument;
+	} else
+	{
+		// Reuse this entry for the new argument
+		argument = current;
+	}
+
+	return argument;
 }
