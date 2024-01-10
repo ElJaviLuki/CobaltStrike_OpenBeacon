10ba3ebdd03128da3d9145e682c20b0d9d049a72
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2024-01-10 02:46:57 +0100

    Implement FilesystemMove

--- Beacon/filesystem.c
+++ Beacon/filesystem.c
@@ -37,3 +37,27 @@ void FilesystemMkdir(char* buffer, int length)
 
 	free(path);
 }
+
+void FilesystemMove(char* buffer, int length)
+{
+#define MAX_SRC 0x2000
+#define MAX_DST 0x2000
+	datap* locals = BeaconDataAlloc(MAX_SRC + MAX_DST);
+	char* src = BeaconDataPtr(locals, MAX_SRC);
+	char* dst = BeaconDataPtr(locals, MAX_DST);
+
+	datap parser;
+	BeaconDataParse(&parser, buffer, length);
+	BeaconDataStringCopySafe(&parser, src, MAX_SRC);
+	BeaconDataStringCopySafe(&parser, dst, MAX_DST);
+
+	// Move the file
+	if(!MoveFileA(src, dst))
+	{
+		DWORD lastError = GetLastError();
+		LERROR("Move failed: %s", LAST_ERROR_STR(lastError));
+		BeaconErrorD(ERROR_MOVE_FAILED, lastError);
+	}
+
+	BeaconDataFree(locals);
+}
