200e6ccd0afb6a447e1959d1a31f70479c440585
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2024-01-22 00:07:59 +0100

    Implement PowershellImport

---
+++ Beacon/powershell.c
@@ -0,0 +1,14 @@
+#include "pch.h"
+
+char* gImportedPshScript;
+
+char* PowershellImport(char* buffer, int size)
+{
+	if (gImportedPshScript)
+		free(gImportedPshScript);
+
+	gImportedPshScript = (char*)malloc(size + 1);
+	memcpy(gImportedPshScript, buffer, size);
+	gImportedPshScript[size] = 0;
+	return gImportedPshScript;
+}
