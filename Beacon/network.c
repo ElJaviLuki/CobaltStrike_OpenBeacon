d3a79c36cbcdc993d29a52f826345b707f45af98
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2024-01-30 23:41:55 +0100

    Implement NetworkGetActiveAdapterIPv4

--- Beacon/network.c
+++ Beacon/network.c
@@ -20,3 +20,32 @@ void NetworkInit(void)
 
 	gNetworkIsInit = TRUE;
 }
+
+ULONG NetworkGetActiveAdapterIPv4()
+{
+	SOCKET sock = WSASocketA(AF_INET, SOCK_DGRAM, 0, NULL, 0, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		return 0;
+	}
+
+	DWORD bytesReturned;
+	int numInterfaces = 0;
+	INTERFACE_INFO interfaceInfo[20];
+	if (!WSAIoctl(sock, SIO_GET_INTERFACE_LIST, NULL, 0, interfaceInfo, sizeof(interfaceInfo), &bytesReturned, NULL, NULL))
+	{
+		numInterfaces = bytesReturned / sizeof(INTERFACE_INFO);
+	}
+
+	for (int i = 0; i < numInterfaces; i++)
+	{
+		if (!(interfaceInfo[i].iiFlags & IFF_LOOPBACK) && interfaceInfo[i].iiFlags & IFF_UP)
+		{
+			closesocket(sock);
+			return interfaceInfo[i].iiAddress.AddressIn.sin_addr.s_addr;
+		}
+	}
+
+	closesocket(sock);
+	return 0;
+}
--- Beacon/network.h
+++ Beacon/network.h
@@ -4,3 +4,5 @@
 #define LOCALHOST 0x0100007f
 
 void NetworkInit(void);
+
+ULONG NetworkGetActiveAdapterIPv4();
