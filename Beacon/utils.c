51059e98ebe00489e9e5ab9a31ea575842d33c9a
Javier <42473691+ElJaviLuki@users.noreply.github.com>
2024-01-31 22:07:45 +0100

    Add Data Conversion Utils

--- Beacon/utils.c
+++ Beacon/utils.c
@@ -36,3 +36,53 @@ int RandomEvenInt(void)
 {
 	return RoundToNearestEven(RandomInt());
 }
+
+int ToNetbios(const char nb, const char* in, const int inlen, char* out, const int outlen)
+{
+	int i, j;
+	for (i = 0, j = 0; i < inlen && j < outlen; i++, j += 2)
+	{
+		// Extract the upper and lower nibbles from "in"
+		// Calculate the results and store them in "out"
+		out[j] = (char)(in[i] >> 4 & 0x0F) + nb;
+		out[j + 1] = (char)(in[i] & 0x0F) + nb;
+	}
+	return j;
+}
+
+int FromNetbios(const char nb, const char* in, const int inlen, char* out, const int outlen)
+{
+	if (inlen % 2 == 1) return 0;
+
+	int i, j;
+	for (i = 0, j = 0; i < inlen && j < outlen; i++, j += 2)
+	{
+		out[i] = (char)((in[j] - nb) << 4) | (char)(in[j + 1] - nb);
+	}
+
+	return inlen / 2;
+}
+
+#define MASK_SIZE sizeof(int)
+int XorMask(const char* in, const int inlen, char* out, const int outlen)
+{
+	const int outres = inlen + MASK_SIZE;
+	if (outres > outlen)
+		return 0;
+
+	*(int*)out = RandomInt();
+	for (int i = 0; i < inlen; i++)
+		out[i + MASK_SIZE] = in[i] ^ out[i % MASK_SIZE];
+
+	return outres;
+}
+
+int XorUnmask(const char* in, const int inlen, char* out, const int outlen)
+{
+	const int raw_inlen = inlen - MASK_SIZE;
+	if (raw_inlen > outlen)
+		return 0;
+	for (int i = 0; i < raw_inlen; i++)
+		out[i] = in[i + MASK_SIZE] ^ in[i % MASK_SIZE];
+	return raw_inlen;
+}
--- Beacon/utils.h
+++ Beacon/utils.h
@@ -3,3 +3,11 @@
 DWORD ExpandEnvironmentStrings_s(const char* lpSrc, char* lpDst, size_t size);
 
 int RandomEvenInt(void);
+
+int ToNetbios(char nb, const char* in, int inlen, char* out, int outlen);
+
+int FromNetbios(char nb, const char* in, int inlen, char* out, int outlen);
+
+int XorMask(const char* in, int inlen, char* out, int outlen);
+
+int XorUnmask(const char* in, int inlen, char* out, int outlen);
